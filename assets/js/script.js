e358f6a116c7b99c98244f5cf4e6f102f6a1f484
Mason021 <mdc5054@gmail.com>
2021-07-03 15:57:21 -0500

    updated my querySelectors and tied them to class's in the HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,15 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+var cities = [];
+
+var lookUpCityFormEl=document.querySelector("#lookUpCityForm");
+var enteredCityEl=document.querySelector("#enteredcity");
+var lookedUpCityEl=document.querySelector("#lookedUpCity");
+var upcomingWeather=document.querySelector("#upcomingWeather");
+var currentWeatherHolderEl=document.querySelector("#currentWeatherHolder");
+var fiveDayWeatherHolderEl=document.querySelector("#fiveDayWeatherHolder");
+var recentlyViewedEl=document.querySelector("#recentlyViewed");
 
 
 
@@ -22,10 +31,45 @@
 
 
 
-var keyOfAPI = 'cbc3e876c3052d94c4bb2fd2f431468b'
 
-var button = document.querySelector(".button")
-var inputValue = document.querySelector(".inputValue")
+var handlesSubmit = function(event){
+    event.preventDefault();
+    var enteredCity = enteredCityEl.value.trim();
+    if(enteredCity){
+        getWeatherData(enteredCity);
+
+        cities.unshift({cities});
+        enteredCityEl.value = "";
+    } else{
+        alert("Something is off here, please try again");
+    }
+    saveSearch();
+}
+
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+
+var getWeatherData = function(enteredCity){
+    var keyOfAPI = 'cbc3e876c3052d94c4bb2fd2f431468b'
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${enteredCity}&units=imperial&appid=${keyOfAPI}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            showWeather(data, enteredCity);
+        });
+    });
+   
+};
+
+
+
+
+
+
+// var button = document.querySelector(".button")
+// var inputValue = document.querySelector(".inputValue")
 
 
 // var name = document.querySelector(".name");
@@ -43,20 +87,20 @@ var inputValue = document.querySelector(".inputValue")
 
 
 
-button.addEventListener("click", function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid='+keyOfAPI+'&units=imperial')
-    .then(response => response.json())
-    // .then(data => console.log(data))
-    .then(data => {
-        var nameValue = data.name;
-        var tempValue = data.main.temp
-        var descValue = data.weather[0].description
-        var humidValue = data.main.humidity
-        var windSpeedValue = data.wind.speed
-        var mapCordLat = data.coord.lat   
-        var mapCordLon =  data.coord.lon
-        var iconUrl = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-        console.log(data)
+// button.addEventListener("click", function(){
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid='+keyOfAPI+'&units=imperial')
+//     .then(response => response.json())
+//     // .then(data => console.log(data))
+//     .then(data => {
+//         var nameValue = data.name;
+//         var tempValue = data.main.temp
+//         var descValue = data.weather[0].description
+//         var humidValue = data.main.humidity
+//         var windSpeedValue = data.wind.speed
+//         var mapCordLat = data.coord.lat   
+//         var mapCordLon =  data.coord.lon
+//         var iconUrl = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+//         console.log(data)
 
         // name.innerHTML = nameValue;
         // temp.innerHTML = tempValue;
@@ -64,32 +108,32 @@ button.addEventListener("click", function(){
         // humidity.innerHTML = humidValue;
         // windSpeed.innerHTML = windSpeedValue;
 
-        fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+mapCordLat+'&lon='+mapCordLon+'&appid='+keyOfAPI+'&units=imperial')
-        .then(response => response.json())
-        // .then(data => console.log(data))
-        .then(data => {
-            fiveDayForcast(data.daily)
-        })
-    })
+//         fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+mapCordLat+'&lon='+mapCordLon+'&appid='+keyOfAPI+'&units=imperial')
+//         .then(response => response.json())
+//         // .then(data => console.log(data))
+//         .then(data => {
+//             fiveDayForcast(data.daily)
+//         })
+//     })
 
-// .catch(err => alert("Something is off here, please try again")) 
-})
+// // .catch(err => alert("Something is off here, please try again")) 
+// })
 
-function fiveDayForcast(array) {
-    var uvIndexValue = array[1].uvi
-    var dateValue = array[1].dt
-    var dateValueInMill = dateValue * 1000
-    var dateForHumanEyes = new Date(dateValueInMill)
+// function fiveDayForcast(array) {
+//     var uvIndexValue = array[1].uvi
+//     var dateValue = array[1].dt
+//     var dateValueInMill = dateValue * 1000
+//     var dateForHumanEyes = new Date(dateValueInMill)
 
-    var actualDayForHumans = dateForHumanEyes.toString().split("12:00:00")[0]
-    for (i=1; i<=5 ; i++) {
+//     var actualDayForHumans = dateForHumanEyes.toString().split("12:00:00")[0]
+//     for (i=1; i<=5 ; i++) {
        
-    }
+//     }
 
-    console.log(uvIndexValue)
-    console.log(dateValue)
-    console.log(dateForHumanEyes)
-    console.log(actualDayForHumans)
+//     console.log(uvIndexValue)
+//     console.log(dateValue)
+//     console.log(dateForHumanEyes)
+//     console.log(actualDayForHumans)
 }
 
 // fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=cbc3e876c3052d94c4bb2fd2f431468b')
